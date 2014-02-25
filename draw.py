34d1055e8591426848e481f450c608ecd4a68846
Christy O'Reilly <christy@oreills.co.uk>
2014-02-25 21:09:10 +0000

    Actually generate circles

--- draw.py
+++ draw.py
@@ -4,11 +4,11 @@ import simplekml
 kml = simplekml.Kml()
 CIRCLE_VERTS = 40
 
-def draw_circle(name, lat, lon, rad):
+def draw_circle(name, lat, lon, lat_rad, lon_rad):
     ls = kml.newlinestring(name=name)
     pi_points = [2 * math.pi * x/float(CIRCLE_VERTS)
             for x in xrange(CIRCLE_VERTS + 1)]
-    ls.coords = [(rad * math.sin(t) + lon, rad * math.cos(t) + lat)
+    ls.coords = [(lon_rad * math.sin(t) + lon, lat_rad * math.cos(t) + lat)
                 for t in pi_points]
 
 def save_kml():
--- main.py
+++ main.py
@@ -4,15 +4,27 @@ import convert
 from data import get_reachable_stops_from_name
 from draw import draw_circle, save_kml
 
-def draw_reachable_stops_from_name(name, start_rad, end_rad):
-    reachable_stops = get_reachable_stops_from_name(name, start_rad)
+def draw_reachable_stops_from_name(name, rad):
+    reachable_stops = get_reachable_stops_from_name(name, rad)
+    lat_rad, lon_rad = None, None
     for stop in reachable_stops:
+        if not (lat_rad and lon_rad):
+            # Work out radius in latitudinal/longitudinal degrees for this
+            # general area
+            lat_rad, lon_rad = calculate_lat_lon_rads(stop, rad)
         lat, lon = convert.OSGB36toWGS84(stop.x, stop.y)
-        draw_circle(stop.name, lat, lon, end_rad)
+        draw_circle(stop.name, lat, lon, lat_rad, lon_rad)
     save_kml()
+
+def calculate_lat_lon_rads(stop, rad):
+    lat_rad, lon_rad = convert.OSGB36toWGS84(stop.x, stop.y)
+    lat_rad_d, lon_rad_d = convert.OSGB36toWGS84(stop.x-rad, stop.y-rad)
+    lat_rad -= lat_rad_d
+    lon_rad -= lon_rad_d
+    return lat_rad, lon_rad
 
 if __name__ == '__main__':
     import sys
-    _, name, start_rad, end_rad = sys.argv
-    start_rad, end_rad = float(start_rad), float(end_rad)
-    draw_reachable_stops_from_name(name, start_rad, end_rad)
+    _, name, rad = sys.argv
+    rad = float(rad)
+    draw_reachable_stops_from_name(name, rad)
