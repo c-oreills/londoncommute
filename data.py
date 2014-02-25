21c660fc6cb37a0d31a7065808fd8e7bd37a9978
Christy O'Reilly <christy@oreills.co.uk>
2014-02-25 21:10:48 +0000

    Remove geo module

--- data.py
+++ data.py
@@ -1,8 +1,6 @@
 from collections import defaultdict, namedtuple
 from csv import DictReader
 
-from geo import iter_in_rad
-
 
 class fields(object):
     stop_id = 'Stop_Code_LBSL'
@@ -35,6 +33,17 @@ def index_stops(stops):
         stops_by_id[stop.id] = stop
         stops_by_name[stop.name] = stop
     return stops_by_id, stops_by_name
+
+def iter_in_rad(stops, x, y, rad):
+    max_x, min_x = x + rad, x - rad
+    max_y, min_y = y + rad, y - rad
+    rad_sq = rad ** 2
+    for stop in stops:
+        stop_x, stop_y = stop.x, stop.y
+        if not ((min_x, min_y) <= (stop_x, stop_y) <= (max_x, max_y)):
+            continue
+        if (stop_x - x) ** 2 <= rad_sq and (stop_y - y) ** 2 <= rad_sq:
+            yield stop
 
 def get_nearby_stops(stops, x, y, rad):
     return list(iter_in_rad(stops, x, y, rad))
